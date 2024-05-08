77896b374521fe4b0931f9d16f6833ee671b34ea
Hugo Garritsen <42620633+hugogarrix@users.noreply.github.com>
2024-05-08 10:42:11 +0200

    Create Api/Base.vue

---
+++ frontend/src/views/Settings/ControlPanel/Api/Base.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AuthOptions from "@/components/Settings/General/AuthOptions.vue";
+import storeAuth from "@/stores/auth";
+
+// Props
+const auth = storeAuth();
+</script>
+
+<template>
+  <theme />
+  <interface-options class="mt-1" />
+  <task-status v-if="auth.scopes.includes('tasks.run')" class="mt-1" />
+</template>
